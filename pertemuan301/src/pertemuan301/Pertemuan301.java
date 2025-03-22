ab51365d61b74859ff5bd7d2b073755ec7964591
OOP C <arthawebid@gmail.com>
2025-03-22 12:28:56 +0800

    Pertemuan 3

---
+++ pertemuan301/src/pertemuan301/Pertemuan301.java
@@ -0,0 +1,17 @@
+package pertemuan301;
+
+/**
+ *
+ * @author Made Artha
+ * tgl 22 Maret 2025
+ */
+public class Pertemuan301 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
